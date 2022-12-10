88d62b5d6830ffbaa7d8204f84877b8f6027723b
clark-lindsay <clarklindsay96@gmail.com>
2022-12-10 12:58:23 -0500

    Complete day 4

---
+++ day_4/cleaning_assignment_subsets.exs
@@ -0,0 +1,18 @@
+File.stream!(Path.relative("input.txt"))
+|> Stream.map(&String.trim/1)
+|> Stream.filter(&(String.length(&1) > 0))
+|> Task.async_stream(fn pair ->
+  [[start_a, end_a], [start_b, end_b]] =
+    String.split(pair, ",")
+    |> Enum.map(fn assignment ->
+      String.split(assignment, "-")
+      |> Enum.map(&String.to_integer/1)
+    end)
+
+  set_a = MapSet.new(start_a..end_a)
+  set_b = MapSet.new(start_b..end_b)
+
+  MapSet.subset?(set_a, set_b) or MapSet.subset?(set_b, set_a)
+end)
+|> Enum.count(fn {:ok, bool} -> bool end)
+|> IO.inspect(label: "result")
